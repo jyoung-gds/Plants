0d0505d64f7740c38c00c73cc772afae581b0b7c
Johnny Young <94846816+jyoung-gds@users.noreply.github.com>
2022-02-24 15:44:47 +0000

    Update confirmation.js

--- routes/confirmation.js
+++ routes/confirmation.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-var ConfirmationController =  {
-  Confirmation: function(req, res) {
-    res.render('../views/form/confirmation');
-  }
-};
+const ConfirmationController = require('../controllers/confirmation');
+
+router.get('/', ConfirmationController.Confirmation);
 
 module.exports = router;
