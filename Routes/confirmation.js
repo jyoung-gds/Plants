4be93986499675febc0fe172a059dde1fef0736d
Johnny Young <94846816+jyoung-gds@users.noreply.github.com>
2022-02-24 14:51:20 +0000

    Delete Routes directory

--- Routes/confirmation.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var ConfirmationController = require('../controllers/confirmation');
-
-router.get('/',ConfirmationController.Confirmation)
-
-module.exports = router;
--- Routes/filter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const FilterController = require('../controllers/filter');
-
-router.get('/new', FilterController.New);
-
-module.exports = router;
