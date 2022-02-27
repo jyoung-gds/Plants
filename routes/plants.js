012b7372d0ffe862f04ec94c48e422f8468627a7
Justine Williams <just.williams04@gmail.com>
2022-02-27 17:33:57 +0100

    Added a logo and fixing issues with layout

--- routes/plants.js
+++ routes/plants.js
@@ -5,6 +5,7 @@ const PlantsController = require('../controllers/plants');
 
 router.get('/', PlantsController.Index);
 router.get('/doctor', PlantsController.Doctor);
+router.get('/doctor/recieved', PlantsController.Recieved);
 router.get('/:id', PlantsController.Single);
 
 module.exports = router;
