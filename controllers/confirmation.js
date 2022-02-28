34bbbbdae18fb828ebe3249da647b91e744f0bc7
Justine Williams <just.williams04@gmail.com>
2022-02-28 18:22:46 +0100

    added a quiz component when you buy plant

--- controllers/confirmation.js
+++ controllers/confirmation.js
@@ -1,6 +1,38 @@
+/* eslint-disable max-len */
+const Plants = require('../models/plants');
+
 const ConfirmationController = {
   Confirmation: function(req, res) {
     res.render('../views/form/confirmation');
+  },
+
+  Quiz: function(req, res) {
+    res.render('quiz', {plantID: req.params.id});
+  },
+
+  Result: function(req, res) {
+    // eslint-disable-next-line no-var
+    var plantID = req.params.id;
+    Plants.find({_id: plantID}).exec(function(err, results) {
+      if (err) {
+        throw err;
+      }
+
+      // eslint-disable-next-line no-var
+      var dbResultSoil = results[0].Soil_Indicator;
+      // eslint-disable-next-line no-var
+      var dbResultType = results[0].Type;
+
+      // console.log(dbResultSoil);
+      // console.log(req.query.soilIndicator );
+      // console.log(dbResultType);
+      // console.log(req.query.type );
+
+      if (dbResultSoil == req.query.soilIndicator && dbResultType == req.query.type) {
+        res.render('./form/confirmation');
+      }
+      res.render('./form/index');
+    });
   },
 };
 
