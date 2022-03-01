6e07a1bda6466528b675cdc7a8b4c4c9ac71f6b7
Julia.guida <32463353+juliaguida@users.noreply.github.com>
2022-03-01 22:07:29 +0000

    User password encrypted

--- models/user.js
+++ models/user.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
   email: String,
   password: String,
 
+});
+
+UserSchema.pre('save', async function(next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 const User = mongoose.model('User', UserSchema);
