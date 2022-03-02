ef08123169a28fa9f1f4eeedc3547bb4bd6c898c
Justine Williams <just.williams04@gmail.com>
2022-03-02 15:33:43 +0000

    created mapBox

--- routes/map.js
+++ routes/map.js
@@ -4,8 +4,5 @@ const router = express.Router();
 const MapsControler = require('../controllers/maps');
 
 router.get('/', MapsControler.Index);
-router.get('/test', function(req, res) {
-  console.log(req.body);
-});
 
 module.exports = router;
